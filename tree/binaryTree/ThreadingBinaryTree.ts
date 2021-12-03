733cb6924c0ee2785ad8c060fa6f4e6045b76c7d
litong <tjlitong@travelsky.com>
2021-12-03 16:38:37 +0800

    feat:enhancement

--- tree/binaryTree/ThreadingBinaryTree.ts
+++ tree/binaryTree/ThreadingBinaryTree.ts
@@ -1,87 +1,84 @@
+enum ThreadTag {
+    Child,
+    Thread
+}
+
 export interface TheadingBinaryTreeNode<T> {
     data: T;
-    // 0: Child Element; 1: Previous Element
-    lTag: 0 | 1;
-    // 0: Child Element; 1: Next Element
-    rTag: 0 | 1;
+    lTag: ThreadTag;
+    rTag: ThreadTag;
     lChild?: TheadingBinaryTreeNode<T>;
     rChild?: TheadingBinaryTreeNode<T>;
 }
 
 const normalBinaryTree: TheadingBinaryTreeNode<string> = {
     data: 'A',
-    lTag: 0,
-    rTag: 0,
+    lTag: ThreadTag.Child,
+    rTag: ThreadTag.Child,
     lChild: {
         data: 'B',
-        lTag: 0,
-        rTag: 0,
+        lTag: ThreadTag.Child,
+        rTag: ThreadTag.Child,
         lChild: {
-            lTag: 0,
-            rTag: 0,
+            lTag: ThreadTag.Child,
+            rTag: ThreadTag.Child,
             data: 'D'
         },
         rChild: {
             data: 'F',
-            lTag: 0,
-            rTag: 0,
+            lTag: ThreadTag.Child,
+            rTag: ThreadTag.Child,
             lChild: {
-                lTag: 0,
-                rTag: 0,
+                lTag: ThreadTag.Child,
+                rTag: ThreadTag.Child,
                 data: 'E'
             }
         }
     },
     rChild: {
         data: 'C',
-        lTag: 0,
-        rTag: 0,
+        lTag: ThreadTag.Child,
+        rTag: ThreadTag.Child,
         lChild: {
             data: 'G',
-            lTag: 0,
-            rTag: 0,
+            lTag: ThreadTag.Child,
+            rTag: ThreadTag.Child,
             rChild: {
-                lTag: 0,
-                rTag: 0,
+                lTag: ThreadTag.Child,
+                rTag: ThreadTag.Child,
                 data: 'H'
             }
         },
         rChild: {
-            lTag: 0,
-            rTag: 0,
+            lTag: ThreadTag.Child,
+            rTag: ThreadTag.Child,
             data: 'I'
         }
     }
 };
 
-function inOrderRecursion<T>(tree: TheadingBinaryTreeNode<T> | undefined, list: TheadingBinaryTreeNode<T>[]) {
+let preEle: TheadingBinaryTreeNode<any> | undefined;
+
+function theadlize<T>(tree: TheadingBinaryTreeNode<T> | undefined) {
     if (!tree) return;
-    inOrderRecursion(tree.lChild, list);
-    list.push(tree);
-    inOrderRecursion(tree.rChild, list);
-}
 
-function theadlize<T>(tree: TheadingBinaryTreeNode<T>) {
-
-    const treelist: TheadingBinaryTreeNode<T>[] = [];
-    inOrderRecursion(tree, treelist);
-
-    treelist.forEach((item, index) => {
-        const preiousEle = treelist[index - 1];
-        const nextEle = treelist[index + 1];
-        if (preiousEle && !item.lChild) {
-            item.lChild = preiousEle;
-            item.lTag = 1;
+    theadlize(tree.lChild);
+    if (!tree.lChild) {
+        tree.lTag = ThreadTag.Thread;
+        tree.lChild = preEle;
+    }
+    if (preEle) {
+        if (!preEle.rChild) {
+            preEle.rTag = ThreadTag.Thread;
+            preEle.rChild = tree;
         }
+    }
 
-        if (nextEle && !item.rChild) {
-            item.rChild = nextEle;
-            item.rTag = 1;
-        }
-    })
+    preEle = tree;
+
+    theadlize(tree.rChild);
 }
 
 theadlize(normalBinaryTree);
-
 
 console.log(normalBinaryTree);
